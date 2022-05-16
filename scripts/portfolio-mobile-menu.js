7807eae6bdca66a5c485ec474ddaeb537a5b5abb
jorgegoco <jorgegoco70@gmail.com>
2022-05-16 19:36:10 +0200

    Create a JavaScrip folder

---
+++ scripts/portfolio-mobile-menu.js
@@ -0,0 +1,16 @@
+function showMenu() {
+    document.querySelector('#menu-mobile').style.display = 'block';
+    document.querySelector('.contain-menu-link').style.display = 'flex';
+}
+
+function hideMenu() {
+    document.querySelector('#menu-mobile').style.display = 'none';
+}
+
+document.getElementById("hamburger-icon").addEventListener('click', showMenu);
+document.getElementById("close-button").addEventListener('click', hideMenu);
+/*let menuLinks = Array.from(document.querySelectorAll(".menu-link"));
+for (let i = 0; i < menuLinks.length; i++) {
+    menuLinks[i].addEventListener('click', hideMenu);
+  }*/
+  
