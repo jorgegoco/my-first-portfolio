9d6a088fb9d00aef517f25663181cfae3850164f
jorgegoco <jorgegoco70@gmail.com>
2022-05-18 19:47:20 +0200

    Fix branch error

--- scripts/project-details.js
+++ scripts/project-details.js
@@ -1,8 +1,69 @@
-function showWork() {
-    document.querySelector('#show-work').style.display = 'block';
-  }
-  
-  let workLinks = Array.from(document.querySelectorAll(".work-button"));
-  for (let i = 0; i < workLinks.length; i++) {
-      workLinks[i].addEventListener('click', showWork);
-    }
+const projects = [
+  {
+    id: 1,
+    title: 'Multi-Post Stories Gain+Glory',
+    img_desktop: './images/my-works-desktop.png',
+    img_mobile: './images/my-works-mobile.png',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    liveLink: 'https://jorgegoco.github.io/my-first-portfolio/',
+    liveSource: 'https://github.com/jorgegoco/my-first-portfolio',
+    techstack_desktop: ['Codekit','Github','Javascript','Bootstrap','Terminal','Codepen'],
+    techstack_mobile: ['Ruby on rails','CSS','Javascript'],
+  },
+  {
+    id: 2,
+    title: 'Multi-Post Stories Gain+Glory',
+    img_desktop: './images/my-works-desktop.png',
+    img_mobile: './images/my-works-mobile.png',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    liveLink: 'https://jorgegoco.github.io/my-first-portfolio/',
+    liveSource: 'https://github.com/jorgegoco/my-first-portfolio',
+    techstack_desktop: ['Codekit','Github','Javascript','Bootstrap','Terminal','Codepen'],
+    techstack_mobile: ['Ruby on rails','CSS','Javascript'],
+  },
+  {
+    id: 3,
+    title: 'Multi-Post Stories Gain+Glory',
+    img_desktop: './images/my-works-desktop.png',
+    img_mobile: './images/my-works-mobile.png',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    liveLink: 'https://jorgegoco.github.io/my-first-portfolio/',
+    liveSource: 'https://github.com/jorgegoco/my-first-portfolio',
+    techstack_desktop: ['Codekit','Github','Javascript','Bootstrap','Terminal','Codepen'],
+    techstack_mobile: ['Ruby on rails','CSS','Javascript'],
+  },
+  {
+    id: 4,
+    title: 'Multi-Post Stories Gain+Glory',
+    img_desktop: './images/my-works-desktop.png',
+    img_mobile: './images/my-works-mobile.png',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    liveLink: 'https://jorgegoco.github.io/my-first-portfolio/',
+    liveSource: 'https://github.com/jorgegoco/my-first-portfolio',
+    techstack_desktop: ['Codekit','Github','Javascript','Bootstrap','Terminal','Codepen'],
+    techstack_mobile: ['Ruby on rails','CSS','Javascript'],
+  },
+  {
+    id: 5,
+    title: 'Multi-Post Stories Gain+Glory',
+    img_desktop: './images/my-works-desktop.png',
+    img_mobile: './images/my-works-mobile.png',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    liveLink: 'https://jorgegoco.github.io/my-first-portfolio/',
+    liveSource: 'https://github.com/jorgegoco/my-first-portfolio',
+    techstack_desktop: ['Codekit','Github','Javascript','Bootstrap','Terminal','Codepen'],
+    techstack_mobile: ['Ruby on rails','CSS','Javascript'],
+  },
+  {
+    id: 6,
+    title: 'Multi-Post Stories Gain+Glory',
+    img_desktop: './images/my-works-desktop.png',
+    img_mobile: './images/my-works-mobile.png',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+    liveLink: 'https://jorgegoco.github.io/my-first-portfolio/',
+    liveSource: 'https://github.com/jorgegoco/my-first-portfolio',
+    techstack_desktop: ['Codekit','Github','Javascript','Bootstrap','Terminal','Codepen'],
+    techstack_mobile: ['Ruby on rails','CSS','Javascript'],
+  },
+
+];
